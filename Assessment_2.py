93c942f082dd4ad64688d00db7cdc756450d4ce1
gowthammuthuraman <gowtham.muthuraman@live.vu.edu.au>
2022-05-24 21:19:05 +1000

    Average Salary of Manager Group employees calculated

---
+++ Assessment_2.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+
+Emp = pd.read_csv('employees.csv')
+# Loaded employees csv into a pandas dataframe
+E = Emp[Emp['employee_type'] == "Manager"]
+# Filtered the employees Dataframe based on employee type
+Avg = E['salary'].mean()
+# Average of Manager group employee_type
+Low_Salary = Emp['salary'].min()
+print(Low_Salary)
+
