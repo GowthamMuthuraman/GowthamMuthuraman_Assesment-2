e3fb8296b84c7d268e96958ef864fe544b8e7c36
gowthammuthuraman <gowtham.muthuraman@live.vu.edu.au>
2022-05-24 21:29:52 +1000

    Average Salary of Manager Group employees calculated Lowest salary calculated Details of the lowest salaried employee calculated

--- Assessment_2.py
+++ Assessment_2.py
@@ -5,10 +5,14 @@ Emp = pd.read_csv('employees.csv')
 # Loaded employees csv into a pandas dataframe
 E = Emp[Emp['employee_type'] == "Manager"]
 # Filtered the employees Dataframe based on employee type
-Avg = E['salary'].mean()
+Avg = E['salary'].mean().astype(int)
 # Average of Manager group employee_type
 Low_Salary = Emp['salary'].min()
-print(Low_Salary)
-
 Low_Salary_Record = Emp[Emp['salary'] == Low_Salary]
 #  Details of the lowest salaried employee
+
+First_name = Low_Salary_Record['first_name'].item()
+Last_name = Low_Salary_Record['last_name'].item()
+
+print("Average Salary of Manager group:", Avg)
+print(First_name,Last_name, "has the lowest salary")
